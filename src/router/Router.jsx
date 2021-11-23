8eca71588289702dc557955b6951ed031a7837ec
mikit61 <94412432+mikit61@users.noreply.github.com>
2021-11-23 14:02:55 +0900

    セクション7: グローバルなstate管理を知る

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,4 @@
-import { Route, Switch } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import { Top } from "../components/pages/Top";
 import { Users } from "../components/pages/Users";
 import { DefaultLayout } from "../components/templates/DefaultLayout";
